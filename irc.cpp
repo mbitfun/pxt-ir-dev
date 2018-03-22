715aa9e56e1c1943bb9afe5a0332fe82fbed2c28
ohteki <ohteki@syun.jp>
2018-03-22 12:29:29 +0900

    cpp test

--- irc.cpp
+++ irc.cpp
@@ -1,13 +1,16 @@
 #include "pxt.h"
 
+MicroBit uBit;
+
 using namespace pxt;
 
 namespace IRC {
 
   //%
   void init(){
+    uBit.init();
 
-    display.scroll("HELLO WORLD! :)");
+    uBit.display.scroll("HELLO WORLD! :)");
 
     release_fiber();
 	}
