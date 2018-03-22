1101aef5e07d0302c698f9160e82d349f73d48f1
ohteki <ohteki@syun.jp>
2018-03-22 11:00:04 +0900

    IDE

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% weight=70 icon="\u30A2" color=#EC7505 block="赤外線"
-namespace katakana {
-  //% blockId=show_strings block="テスト %v"
-  export function showString(text: string, time: number = 500): void {
+namespace irc {
+  //% blockId=show_strings block="テスト"
+  export function getir(): void {
     basic.showLeds(`
       . . . . .
       . # . # .
