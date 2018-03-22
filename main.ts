2620c32e4468d6ad32e6456e75a4b00420fa41aa
ohteki <ohteki@syun.jp>
2018-03-22 10:56:21 +0900

    IDE

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
+//% weight=70 icon="\u30A2" color=#EC7505 block="赤外線"
+namespace katakana {
+  //% blockId=show_strings block="テスト %v"
+  export function showString(text: string, time: number = 500): void {
+    basic.showLeds(`
+      . . . . .
+      . # . # .
+      . . . . .
+      # . . . #
+      . # # # .
     `);
+  }
+}
