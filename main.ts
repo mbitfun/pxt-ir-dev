a96fcea428bbe53037aed72a4a8bd0077675e46e
ohteki <ohteki@syun.jp>
2018-03-22 11:16:23 +0900

    cpp test

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
 //% weight=70 icon="\u30A2" color=#EC7505 block="赤外線"
 namespace irc {
   //% blockId=show_strings block="テスト"
+  //% blockId=init
+  //% icon="\uf1ec"
+  //% shim=OLED::init
   export function getir(): void {
-    basic.showLeds(`
-      . . . . .
-      . # . # .
-      . . . . .
-      # . . . #
-      . # # # .
-    `);
+    return;
   }
 }
