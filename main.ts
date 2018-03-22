e8c8fa60f42390f5ea3c5ea7f4094ed93eb0d126
ohteki <ohteki@syun.jp>
2018-03-22 12:28:50 +0900

    cpp test

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% weight=70 icon="\u30A2" color=#EC7505 block="赤外線"
 namespace IRC {
-  //% blockId=show_strings block="テスト"
+  //% block="テスト"
   //% blockId=init
   //% icon="\uf1ec"
   //% shim=IRC::init
