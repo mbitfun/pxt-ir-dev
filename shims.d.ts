4b0804d37888a11d87e069eaaa097b926177448a
ohteki <ohteki@syun.jp>
2018-03-27 13:12:24 +0900

    test

--- shims.d.ts
+++ shims.d.ts
@@ -1,12 +1,9 @@
 // Will be auto-generated if needed.
 //% weight=70 icon="\u30A2" color=#EC7505 block="赤外線"
 namespace IRC {
-  //% block="テスト"
-  //% blockId=init
+  //% block="テスト2"
+  //% blockId=init2
   //% icon="\uf1ec"
   //% shim=IRC::init
-  export function test2(): void {
-    console.log("test")
-    return;
-  }
+  export function test2(): void;
 }
