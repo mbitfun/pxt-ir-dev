79ca861cce8acff2931f1aae1395797bbc244d31
ohteki <ohteki@syun.jp>
2018-03-27 12:50:43 +0900

    test

--- shims.d.ts
+++ shims.d.ts
@@ -1,1 +1,12 @@
 // Will be auto-generated if needed.
+//% weight=70 icon="\u30A2" color=#EC7505 block="赤外線"
+namespace IRC {
+  //% block="テスト"
+  //% blockId=init
+  //% icon="\uf1ec"
+  //% shim=IRC::init
+  export function init(): void {
+    console.log("test")
+    return;
+  }
+}
